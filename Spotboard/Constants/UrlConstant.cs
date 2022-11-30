1dc115490b12db8a338fb6e41939f3fa2372f420
Luiz Camargo <luiz.zito@hotmail.com>
2022-11-30 18:06:35 +0000

    fix url

--- Spotboard/Constants/UrlConstant.cs
+++ Spotboard/Constants/UrlConstant.cs
@@ -1,7 +1,7 @@
 ﻿namespace Spotboard.Constants; 
 public static class UrlConstant 
 { 
-    public const string RedirectUri = "https://localhost:7008/home";
+    public const string RedirectUri = "https://spotboard.azurewebsites.net/home";
     public const string AuthBaseUri = "https://accounts.spotify.com";
     public const string APIBaseUri = "https://api.spotify.com/v1";
     public const string GetTokenEndpoint = "/api/token";
